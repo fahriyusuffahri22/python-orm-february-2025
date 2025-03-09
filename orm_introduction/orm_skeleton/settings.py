0ab32916fea57510568bf3fc06662900cb2efe83
fahriyusuffahri22 <fahriyusuffahri@proton.me>
2025-03-09 15:22:55 +0200

    Update orm_introduction

--- orm_introduction/orm_skeleton/settings.py
+++ orm_introduction/orm_skeleton/settings.py
@@ -76,27 +76,15 @@ WSGI_APPLICATION = 'orm_skeleton.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.postgresql',
-         'NAME': 'orm_introduction_database',
-         'USER': 'postgres',
-         'PASSWORD': '12345',
-         'HOST': '127.0.0.1',
-         'PORT': '5432'
-     }
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "orm_introduction_lab_db",
+        "USER": "postgres",
+        "PASSWORD": "12345",
+        "HOST": "localhost",
+        "PORT": "5432",
+    }
 }
-
-
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": "",
-#         "USER": "",
-#         "PASSWORD": "",
-#         "HOST": "localhost",
-#         "PORT": "5432",
-#     }
-# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
