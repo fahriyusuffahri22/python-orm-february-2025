bf13a4afed6e4cdf58a0f72cdcb4f7afe8d5cae0
fahriyusuffahri22 <fahriyusuffahri@proton.me>
2025-03-22 13:46:28 +0200

    Add advanced_queries_in_django/exercise

---
+++ advanced_queries_in_django/exercise/main_app/validators.py
@@ -0,0 +1,14 @@
+from django.core.exceptions import ValidationError
+from django.utils.deconstruct import deconstructible
+
+
+@deconstructible
+class RangeValidator:
+    def __init__(self, min_value, max_value, message):
+        self.min_value = min_value
+        self.max_value = max_value
+        self.message = message
+
+    def __call__(self, value):
+        if not self.min_value <= value <= self.max_value:
+            raise ValidationError(message=self.message)
