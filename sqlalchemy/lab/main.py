4bcfff795d453d1636d95cedd79c6f6e0b1896a7
fahriyusuffahri22 <fahriyusuffahri@proton.me>
2025-03-23 12:35:57 +0200

    Add sqlalchemy/lab

---
+++ sqlalchemy/lab/main.py
@@ -0,0 +1,7 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = 'postgresql+psycopg2://postgres:12345@127.0.0.1/sqlalchemy_lab_db'
+engine = create_engine(DATABASE_URL, pool_size=10, max_overflow=20)
+
+Session = sessionmaker(bind=engine)
---
+++ sqlalchemy/lab/models.py
@@ -0,0 +1,21 @@
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy.orm import declarative_base, relationship
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String)
+    email = Column(String)
+
+
+class Order(Base):
+    __tablename__ = 'orders'
+
+    id = Column(Integer, primary_key=True)
+    is_completed = Column(Boolean, default=False)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    user = relationship('User')
